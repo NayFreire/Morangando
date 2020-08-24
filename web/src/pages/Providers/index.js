991f3b5036d68ed540ff0f35ecdcfe5285a6886d
matheus simoes <matheusaugustosimoes@hotmail.com>
2020-08-23 22:43:57 -0300

    tela lavouras feita

--- web/src/pages/Providers/index.js
+++ web/src/pages/Providers/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiArrowUp,FiArrowDown } from "react-icons/fi"
+import { FiMessageCircle,FiMail } from "react-icons/fi"
+import { FaPlus } from "react-icons/fa"
 import './styles.css'
 
 
@@ -8,8 +9,268 @@ export default function Providers(){
         <div className="main-container">
             <div className="row">
                  <h2 className="container-title">Fornecedores</h2>
+                 <button className="top-button"><FaPlus/> <p>Adicionar Fornecedor </p></button>
             </div>
        
+            <div className="card-container">
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Maria do Rosário</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiMail size={22}/>
+                                <h3>mariadorosario@gmail.com</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiMessageCircle size={22}/>
+                                <h3>(35)99898-9988</h3>
+                            </div>
+
+                            
+                    </div>
+                </div>
+            </div>
+
+                
         </div>
     );
 }
--- web/src/pages/Tillage/index.js
+++ web/src/pages/Tillage/index.js
@@ -1,15 +1,277 @@
 import React from 'react';
-import { FiArrowUp,FiArrowDown } from "react-icons/fi"
+import { FiCalendar } from "react-icons/fi"
+import { FaPlus } from "react-icons/fa"
 import './styles.css'
 
 
-export default function Tillage(){
+export default function Providers(){
     return(
         <div className="main-container">
-        <div className="row">
-             <h2 className="container-title">Lavouras</h2>
+            <div className="row">
+                 <h2 className="container-title">Lavouras</h2>
+                 <button className="top-button"><FaPlus/> <p>Adicionar Lavoura </p></button>
+            </div>
+       
+            <div className="card-container">
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+                <div className="providers-card">
+                        <h2 className="providers-card-title">Lavoura de Milho</h2>
+                        <p className="providers-card-adress">Bairro capelinha<br/>Bom Repouso</p>
+                    
+                    <div className="info-div">
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Iniciou em 12/08/2020</h3>
+                            </div>
+                            <div className="providers-card-info">
+                                <FiCalendar size={22}/>
+                                <h3>Terminou em --/--/--</h3>
+                            </div>
+
+                             <button className="tillage-button">Ver Detalhes</button>   
+                    </div>
+                </div>
+             
+            </div>
+
+                
         </div>
-   
-    </div>
     );
 }
